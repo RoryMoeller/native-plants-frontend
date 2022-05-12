0d714c55144cf6d390bc31d98a30ea73e605092b
Hunteret7 <hunteret@oregonstate.edu>
2022-05-12 16:17:16 -0700

    lat long

--- src/pages/add-site.js
+++ src/pages/add-site.js
@@ -8,17 +8,23 @@ function Sites() {
     const [ownername, setOwnername] = useState("");
     const [sitename, setSitename] = useState("");
     const [aCode, setACode] = useState("");
+    const [lat, setLat] = useState("");
+    const [long, setLong] = useState("");
+    const [rCode, setRCode] = useState("");
 
     async function postSite(e) {
         e.preventDefault();
+        //let point = '(' + lat + ',' + long + ')'
+        let point = lat +'@@' + long 
         //const res = await fetch('/api/accessBackend/https://native-plants-backend.herokuapp.com/i/INSERT INTO rev2.farms(farm_name, contact_email) VALUES (%s) /'+farmname+', '+farmeamil,{
         const res = await fetch('/api/accessBackend', {
             method: 'POST',
             body: JSON.stringify( {
                 table_name: "site",
                 query_type: "INSERT",
-                query_fields: ['owner_username','collection_site_name','accession_code'],
-                query_values: [ownername, sitename,aCode]
+                query_fields: ['owner_username','collection_site_name','accession_code','region_code','collection_site_lat_long'],
+                query_values: [ownername, sitename,aCode,rCode, point],
+                has_point: true
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -47,6 +53,22 @@ function Sites() {
                     value={sitename}
                     />
             </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Latitude"
+                    onChange={e => setLat(e.target.value)}
+                    value={lat}
+                    />
+            </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Longitude"
+                    onChange={e => setLong(e.target.value)}
+                    value={long}
+                    />
+            </div>
             <p>What is the accession code</p>
             <label className="container">Washington
                 <input type="radio" name="radio" value="WA" onClick={e => setACode(e.target.value)}/>
@@ -66,6 +88,43 @@ function Sites() {
             </label>
             <label className="container">California
                 <input type="radio" name="radio" value="CA" onClick={e => setACode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <p>What is the region code</p>
+            <label className="container">Westport, WA - Ilwaco, WA
+                <input type="radio" name="region" value="LB" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Astoria, OR - Seaside, OR
+                <input type="radio" name="region" value="CP" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Seaside, OR - Tillamook, OR
+                <input type="radio" name="region" value="NT" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Lincoln City, OR - Waldport, OR
+                <input type="radio" name="region" value="WP" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Waldport, OR - Florence, OR
+                <input type="radio" name="region" value="FL" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Florence, OR - Coos Bay, OR
+                <input type="radio" name="region" value="CB" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Coos Bay, OR - Port Ordford, OR
+                <input type="radio" name="region" value="PO" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Port Ordford, OR - Brookings, OR
+                <input type="radio" name="region" value="BR" onClick={e => setRCode(e.target.value)}/>
+                <span className="checkmark"></span>
+            </label>
+            <label className="container">Brookings, OR - Crescent City, OR
+                <input type="radio" name="region" value="LE" onClick={e => setRCode(e.target.value)}/>
                 <span className="checkmark"></span>
             </label>
             <div>
